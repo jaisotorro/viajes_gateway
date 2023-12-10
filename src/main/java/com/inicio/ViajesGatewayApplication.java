018192611b0ee8443bbc8ee064e7c579029aa271
jaimesto <jaimesto@ext.inditex.com>
2023-12-10 19:46:29 +0100

    Initial commit

---
+++ src/main/java/com/inicio/ViajesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesGatewayApplication.class, args);
+	}
+
+}
